a104bd2031c6c8c18597b62da14a7fec93d9f5f7
Radosław <radoslaw@dziadosz.eu>
2016-09-18 15:21:11 +0300

    AutoCompleteTextView, Toast

--- app/src/main/java/eu/dziadosz/basicuicontrols2/MainActivity.java
+++ app/src/main/java/eu/dziadosz/basicuicontrols2/MainActivity.java
@@ -2,6 +2,11 @@ package eu.dziadosz.basicuicontrols2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // AutoCompleteTextView
+        AutoCompleteTextView actv = (AutoCompleteTextView)
+                findViewById(R.id.login); // add stings to control
+        ArrayAdapter<String> aa = new ArrayAdapter<>(this,
+                android.R.layout.simple_dropdown_item_1line,
+                new String[]{"Pasi", "Juha", "Kari", "Jouni", "Esa", "Hannu"});
+        actv.setAdapter(aa);
+
+    }
+
+    public void sendButtonClicked(View view) {
+
+        AutoCompleteTextView login = (AutoCompleteTextView) findViewById(R.id.login);
+        TextView pass = (TextView) findViewById(R.id.password);
+
+        String text = "L: " + login.getText().toString() + "\nP: " + pass.getText().toString();
+
+        Toast.makeText(getApplicationContext(), text,
+                Toast.LENGTH_SHORT).show();
     }
 }
